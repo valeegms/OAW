8627387bb85e128f80f60731c8d20d01aaa1076b
Raúl Villanueva <a16001061@alumnos.uady.mx>
2022-03-08 12:12:15 -0600

    Duplicate URLs are not allowed

---
+++ assets/php/query_sql.php
@@ -0,0 +1,20 @@
+<?php
+function ConsultarSQL ($servidor, $usuario, $contrasena, $basedatos, $sentenciaSQL){
+    $conexion = mysqli_connect($servidor, $usuario, $contrasena, $basedatos);
+    if (!$conexion) {
+        die("Fallo: " . mysqli_connect_error());
+    }
+
+    $resultados = mysqli_query($conexion, $sentenciaSQL);
+
+    $registros = array();
+    while ($row = mysqli_fetch_assoc($resultados)) {
+        $registros[] = $row;
+    }
+
+    mysqli_close($conexion);
+
+    return $registros;
+}
+
+?>
