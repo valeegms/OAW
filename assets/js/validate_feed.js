4252cd05b9aad7dc09572d58975db5b1e62267e8
Raúl Villanueva <a16001061@alumnos.uady.mx>
2022-03-08 09:17:51 -0600

    Feed URL has been validated

--- assets/js/validate_feed.js
+++ assets/js/validate_feed.js
@@ -1,12 +1,19 @@
 let form = document.getElementById("add");
+
 form.onsubmit = () => {
+
     if(form.elements['url'].value == "") {
         form.elements['url'].focus();
         return false;
-    }
-    if (new URL(form.elements['url'].value)==null){
-        alert("Ingrese una URL v&aacutelida");
-        return false;
+    }else{
+        try {
+            new URL(form.elements['url'].value);
+        } catch (_) {
+            alert("Ingrese una URL válida");
+            form.elements['url'].focus();
+            form.elements['url'].select();
+            return false;
+        }
     }
 
     if(form.elements['categoria'].value == "") {
