f534c62331cafd8f7b1b64e418263f6fa45f144d
Raúl Villanueva <a16001061@alumnos.uady.mx>
2022-03-08 12:12:15 -0600

    Duplicate URLs are not allowed

--- assets/js/validate_feed.js
+++ assets/js/validate_feed.js
@@ -1,9 +1,28 @@
 let form = document.getElementById("add");
 
+function verificarURL(url){
+    let xhttp = new XMLHttpRequest();
+    let flag;
+
+    xhttp.onload = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            if(this.responseText != "0"){
+                flag = false;
+            }else{
+                flag= true;
+            }
+        }
+    }
+    xhttp.open("GET", "assets/php/verify_URL_repeated.php?url="+url,false);
+    xhttp.send();
+    return flag;
+}
 form.onsubmit = () => {
+    let url = form.elements['url'].value;
 
     if(form.elements['url'].value == "") {
         form.elements['url'].focus();
+        alert("Debe ingresar una URL")
         return false;
     }else{
         try {
@@ -18,6 +37,13 @@ form.onsubmit = () => {
 
     if(form.elements['categoria'].value == "") {
         form.elements['categoria'].focus();
+        return false;
+    }
+
+    if(!verificarURL(url)){
+        alert("La URL ya ha sido registrada");
+        form.elements['url'].focus();
+        form.elements['url'].select();
         return false;
     }
 }
