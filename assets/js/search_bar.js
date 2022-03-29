f9faf2ead59f93d272b3171f03800e073b2a1cd6
GenerEcheverria <generalex@live.com.mx>
2022-03-28 20:17:10 -0600

    js

--- assets/js/search_bar.js
+++ assets/js/search_bar.js
@@ -1,17 +1,1 @@
-let search = document.getElementById("search-input");
-search.addEventListener("input", function() {
-    let input = search.value;
-    let titles = document.getElementsByClassName("title");
-    let news = document.getElementsByClassName("news");
-
-    for (let i = 0; i < news.length; i++) {
-        news[i].style.setProperty("display","none","important")
-    }
-
-    for (let i = 1; i < titles.length;i++) {
-        if (titles[i].innerHTML.toLowerCase().includes(input.toLowerCase())) {
-            news[i-1].style.removeProperty("display");
-        }
-    }
-})
-
+var c=document.getElementById("search-input");c.addEventListener("input",function(){for(var e=c.value,d=document.getElementsByClassName("title"),b=document.getElementsByClassName("news"),a=0;a<b.length;a++)b[a].style.setProperty("display","none","important");for(a=1;a<d.length;a++)d[a].innerHTML.toLowerCase().includes(e.toLowerCase())&&b[a-1].style.removeProperty("display")});
